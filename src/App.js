8a913d3999cd5a26b28b775fa3c5b7f24f5a090d
ebanguera <ebanguera@unal.edu.co>
2023-09-16 03:43:57 -0500

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <img src="/img/header.png" alt="org" />
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
