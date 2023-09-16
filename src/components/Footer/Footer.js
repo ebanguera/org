d7cc8324cbb63449f81ad7d23ca70f1599ae55d2
ebanguera <ebanguera@unal.edu.co>
2023-09-16 14:33:04 -0500

    componentes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
                 </a>
             </div>
             <img src="/img/Logo.png" alt="org" />
-            <strong>Desarrollado por Alura</strong>
+            <strong>Edinson Banguera &copy; {new Date().getFullYear()}</strong>
         </footer>
     );
 };
